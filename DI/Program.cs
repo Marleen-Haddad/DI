d4a423cd05c517fee9c95674f4abecb9939bb80b
Marleen-Haddad <110169584+Marleen-Haddad@users.noreply.github.com>
2022-12-11 17:08:26 +0300

    Add log configuration

--- DI/Program.cs
+++ DI/Program.cs
@@ -2,10 +2,15 @@ using DI.InterfacesAndAbstractClasses;
 using DI.Middleware;
 using DI.Services;
 using Microsoft.AspNetCore.Diagnostics;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
+var logger = new LoggerConfiguration()
+  .ReadFrom.Configuration(builder.Configuration)
+  .Enrich.FromLogContext()
+  .CreateLogger();
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog(logger);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -16,7 +21,6 @@ builder.Services.AddTransient<IEmployeeDAL, EmployeeDAL>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
